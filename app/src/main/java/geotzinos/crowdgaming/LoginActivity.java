1d44cb3c64974e248f9d89b589b181ee97213916
George Tzinos <geotzinos@gmail.com>
2016-05-28 22:37:48 +0300

    Login screen design.

--- app/src/main/java/geotzinos/crowdgaming/LoginActivity.java
+++ app/src/main/java/geotzinos/crowdgaming/LoginActivity.java
@@ -3,12 +3,12 @@ package geotzinos.crowdgaming;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.login_activity);
     }
 
 }
