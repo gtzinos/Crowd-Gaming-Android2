dff10d4604c5f50531aeef0e9cff0206ed7c9984
George Tzinos <geotzinos@gmail.com>
2016-05-29 21:58:36 +0300

    MyQuestionnairesActivity initialization

---
+++ app/src/main/java/geotzinos/crowdgaming/Controllers/MyQuestionnairesActivity.java
@@ -0,0 +1,15 @@
+package geotzinos.crowdgaming.Controllers;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import geotzinos.crowdgaming.R;
+
+public class MyQuestionnairesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_questionnaires_activity);
+    }
+}
